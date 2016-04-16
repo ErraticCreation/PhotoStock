afe9c40f788907c6c2c7f260acdc4554d8d65425
Tyler Nixon <ErraticCreation@gmail.com>
2016-04-16 12:40:33 -0400

    Updated database seeds and views styling, refactored views code, and implemented a dynamic bootstrap image carousel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Product.create(:name => "seed-generated product", :description => "I also created this product without usng the HTML
-form!", :image_url => "placeholder.png", :color => "red")
+Product.create(:name => "Sample Product", :description => "Bacon ipsum dolor amet pork loin jerky veniam laborum nostrud eu.", :image_url => "cat05.jpg",
+               :color => "red")
+
+Product.create(:name => "Second Sample Product", :description => "Porchetta sunt deserunt proident, corned beef andouille bacon t-bone ground round frankfurter.",
+               :image_url => "cat01.jpg",
+               :color => "orange")
+
+Product.create(:name => "Third Sample Product", :description => "Salami tail filet mignon exercitation nulla, est jowl culpa.",
+               :image_url => "cat02.jpg",
+               :color => "blue")
+
+Product.create(:name => "Fourth Sample Product", :description => "Shoulder ut elit dolore. Deserunt sunt adipisicing, pastrami ea eu voluptate tri-tip boudin capicola elit nisi doner ribeye.",
+               :image_url => "cat03.jpg",
+               :color => "black")
+
+Product.create(:name => "Fifth Sample Product", :description => "Swine beef ex, alcatra do jerky ham hock pancetta. Frankfurter salami chuck picanha aute enim landjaeger pork tongue beef ribs consectetur.",
+               :image_url => "cat04.jpg",
+               :color => "gray")
