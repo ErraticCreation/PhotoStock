f59edf0dd5b1f7b875642402b9eea2ff5f6b3a89
Tyler Nixon <ErraticCreation@gmail.com>
2016-04-25 10:55:52 -0400

    Updated products page styling

---
+++ app/assets/javascripts/custom_scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    // Give Equal height to products on products page
+    $(window).load(function() {
+        $('.product-layout').responsiveEqualHeightGrid();
+    });
+
+});
