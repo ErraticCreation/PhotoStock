440fa8dc33ada292b99143fde2a8685df7c9c0bc
Tyler Nixon <ErraticCreation@gmail.com>
2016-05-12 14:23:59 -0400

    Installed and configured authentication with CanCanCan gem, and added fading functionality to flash alerts

--- app/assets/javascripts/custom_scripts.js
+++ app/assets/javascripts/custom_scripts.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
+var ready;
+ready = function(){
+    // Fade out alert messages
+    $('.alert').delay(5000).fadeOut(800);
 
     // Give Equal height to products on products page
     $(window).load(function() {
         $('.product-layout').responsiveEqualHeightGrid();
     });
 
-});
+};
+
+// turbolinks compatible document ready function
+$(document).ready(ready);
+$(document).on('page:load', ready);
